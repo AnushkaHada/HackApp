2069321d1f80dda7585d523fd236fac26b957ef4
Anushka Hada <anushka@rachelcarlson-100-64-76-220.resnet.ucsc.edu>
2025-05-17 23:11:50 -0700

    created summary from ChatGpt

--- BiographyGeneration/ChatGPTSummary.py
+++ BiographyGeneration/ChatGPTSummary.py
@@ -1,7 +1,10 @@
 from openai import OpenAI
+# Code 
+
 import pandas as pd
 from dotenv import load_dotenv
 import os
+
 load_dotenv()  # Load variables from .env
 
 client = OpenAI(
@@ -16,9 +19,13 @@ client = OpenAI(
 completion = client.chat.completions.create(
   model="gpt-4o-mini",
   store=True,
+  temperature=0.7,
+  max_tokens=100,
   messages=[
-    {"role": "user", "content": "write a haiku about ai"}
+    {"role": "system", "content": "Talk in first-person casually."},
+    {"role": "system", "content": "Biography should explain what skills they have and what they want to learn."},
+    {"role": "system", "content": "Biography should be made with the intention that person is looking to improve skills through others help."},
   ]
 )
 
-print(completion.choices[0].message)
+print(completion.choices[0].message.content)
